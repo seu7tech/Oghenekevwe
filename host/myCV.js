f07bd6e1f0e92b902b1f0e7e2e49a6d0ceb5c0fb
seu7tech <83725884+seu7tech@users.noreply.github.com>
2021-08-06 04:31:40 -0700

    for hosting

---
+++ host/myCV.js
@@ -0,0 +1,18 @@
+
+let skillsEL = document.querySelector("#skills-el")
+
+
+function skills() {
+
+	console.log("Excellent interpersonal skills and ability to effective work with groups")
+	console.log("Excellent business development and networking skills")
+	console.log("Leadership skills")
+	console.log("Strong verbal communication, research and presentation skills")
+}
+
+function Reference() {
+
+	console.log("ENGR MAXWELL BRAOKUBO - Transmission Company of Nigeria")
+	console.log("Assistant manager, Protection control and Metering")
+	console.log("Maxwellbraokubo@gmail.com")
+}
